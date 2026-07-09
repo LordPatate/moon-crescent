9fd0f1f664042822c58b5abefa54bb5b7759419e
LordPatate <tom.bricaud1@orange.fr>
2026-07-09 16:09:03 +0200

    Consequence of previous commit

--- crescent_app.py
+++ crescent_app.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import pygame
@@ -5,16 +6,13 @@ import pygame
 from app import DEFAULT_DISPLAY_SIZE, App, Coordinate, load_image, proportional_blit
 
 
-def make_moon_background(size: Coordinate = DEFAULT_DISPLAY_SIZE) -> pygame.Surface:
-    bkg = pygame.Surface(size)
-    bkg.fill(pygame.Color("black"))
-    moon_img = load_image(Path("images").joinpath("fullmoon.jpg"), scale=0.125)
-    moon_img.set_colorkey(moon_img.get_at((0, 0)))
-    proportional_blit(moon_img, bkg, 0.5, 0.3)
-    return bkg
+@dataclass
+class Moon:
+    center: Coordinate
+    radius: float
 
 
-class Crescent(App):
+class Crescent(App):    
     def on_resize(self, new_size):
         screen = self.make_screen(new_size)
         bkg = self.make_background(new_size)
@@ -23,10 +21,19 @@ class Crescent(App):
         self.background = bkg
         self.screen = screen
 
+    def make_background(self, size: Coordinate = DEFAULT_DISPLAY_SIZE) -> pygame.Surface:
+        bkg = pygame.Surface(size)
+        bkg.fill(pygame.Color("black"))
+        moon_img = load_image(Path("images").joinpath("fullmoon.jpg"), scale=0.125)
+        moon_img.set_colorkey(moon_img.get_at((0, 0)))
+        moon_rect = proportional_blit(moon_img, bkg, 0.5, 0.3)
+        self.moon = Moon(moon_rect.center, moon_rect.width // 2)
+        return bkg
+
 
 def main():
     pygame.init()
-    app = Crescent(background_factory=make_moon_background)
+    app = Crescent()
     app.run()
     pygame.quit()
 
